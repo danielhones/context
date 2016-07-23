272e9263919a79a7b399358bc53a5d9ed0619384
Daniel Hones <danielhones@gmail.com>
2016-07-23 01:42:14 -0400

    made python tests import new script name, fixed ruby regex matches, found colorizing bug

--- python/tests/test.py
+++ python/tests/test.py
@@ -12,7 +12,8 @@ THIS_FILE_DIR = os.path.dirname(os.path.realpath(__file__))
 context_dir = os.path.abspath(os.path.join(THIS_FILE_DIR, ".."))
 sys.path.append(context_dir)
 
-import context
+import imp
+context = imp.load_source('context', 'pycontext')
 from context import *
 
 
