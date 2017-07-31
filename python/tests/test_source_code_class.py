14a428724b017fdfa31a58d35c8a3fadb3e2508d
Daniel Hones <danielhones@gmail.com>
2017-07-30 22:33:12 -0400

    cleaned up some duplication in test_source_code_class

--- python/tests/test_source_code_class.py
+++ python/tests/test_source_code_class.py
@@ -4,6 +4,7 @@ import unittest
 from io import StringIO
 
 from helpers import *
+from context import LineNoMatcher, Matcher, RegexMatcher, SourceCode
 
 
 IF_ELSE_EXAMPLE = """
@@ -33,47 +34,48 @@ class FakeFile(StringIO):
     name = "test"
 
 
-class TestSourceCodeContextSearchLineNo(unittest.TestCase):
+class SourceCodeTestCase(unittest.TestCase):
+    matcher_type = Matcher
+
+    def assert_context_accurate(self, test_file, look_for):
+        self.source = SourceCode(test_file, self.matcher_type, look_for, filename=test_file.name)
+        self.result = self.source.find_context()
+        print("\nRESULT:\n{}".format(self.result))
+        self.assertEqual(self.result, self.expected)
+
+
+class TestSourceCodeContextSearchLineNo(SourceCodeTestCase):
+    matcher_type = LineNoMatcher
+
     def test_if_else_block_shows_if_line(self):
-        test_file = FakeFile(IF_ELSE_EXAMPLE)
-        source = context.SourceCode(test_file, context.LineNoMatcher, 4, filename=test_file.name)
-        result = source.find_context()
-        expected = ["if a is not None:\n",
-                    '    print("a was something. b ==", b)\n']
-        self.assertEqual(result, expected)
+        self.expected = ["if a is not None:\n",
+                         '    print("a was something. b ==", b)\n']
+        self.assert_context_accurate(FakeFile(IF_ELSE_EXAMPLE), 4)
 
     def test_if_else_block_shows_if_and_else_lines(self):
-        test_file = FakeFile(IF_ELSE_EXAMPLE)
-        source = context.SourceCode(test_file, context.LineNoMatcher, 6, filename=test_file.name)
-        result = source.find_context()
-        expected = ["if a is not None:\n",
-                    "else:\n",
-                    "    b = 42\n"]
-        self.assertEqual(result, expected)
+        self.expected = ["if a is not None:\n",
+                         "else:\n",
+                         "    b = 42\n"]
+        self.assert_context_accurate(FakeFile(IF_ELSE_EXAMPLE), 6)
 
     def test_try_except_else_block_shows_try_line(self):
-        test_file = FakeFile(TRY_EXCEPT_ELSE_EXAMPLE)
-        source = context.SourceCode(test_file, context.LineNoMatcher, 6, filename=test_file.name)
-        result = source.find_context()
-        expected = ["try:\n",
-                    '    print("7 ** 4 ==", c)\n']
-
+        self.expected = ["try:\n",
+                         '    print("7 ** 4 ==", c)\n']
+        self.assert_context_accurate(FakeFile(TRY_EXCEPT_ELSE_EXAMPLE), 6)
 
     def test_try_except_else_block_shows_try_and_except_lines(self):
-        test_file = FakeFile(TRY_EXCEPT_ELSE_EXAMPLE)
-        source = context.SourceCode(test_file, context.LineNoMatcher, 8, filename=test_file.name)
-        result = source.find_context()
-        expected = ['try:\n',
-                    'except Exception as e:\n',
-                    '    c = None\n']
-        self.assertEqual(result, expected)
+        self.expected = ['try:\n',
+                         'except Exception as e:\n',
+                         '    c = None\n']
+        self.assert_context_accurate(FakeFile(TRY_EXCEPT_ELSE_EXAMPLE), 8)
 
     def test_try_except_else_block_shows_try_except_and_else_lines(self):
-        test_file = FakeFile(TRY_EXCEPT_ELSE_EXAMPLE)
-        source = context.SourceCode(test_file, context.LineNoMatcher, 12, filename=test_file.name)
-        result = source.find_context()
-        expected = ['try:\n',
-                    'except Exception as e:\n',
-                    'else:\n',
-                    '    print("Ran successfully")\n']
-        self.assertEqual(result, expected)
+        self.expected = ['try:\n',
+                         'except Exception as e:\n',
+                         'else:\n',
+                         '    print("Ran successfully")\n']
+        self.assert_context_accurate(FakeFile(TRY_EXCEPT_ELSE_EXAMPLE), 12)
+
+
+if __name__ == '__main__':
+    unittest.main()
