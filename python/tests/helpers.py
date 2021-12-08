0ad69a858d26e9fe4301395869b59e89868a5cc7
Daniel Hones <danielhones@gmail.com>
2021-12-07 19:18:28 -0500

    Removed old python and ruby directories

--- python/tests/helpers.py
+++
@@ -1,9 +0,0 @@
-import sys
-import os
-
-THIS_FILE_DIR = os.path.dirname(os.path.realpath(__file__))
-context_dir = os.path.abspath(os.path.join(THIS_FILE_DIR, ".."))
-sys.path.append(context_dir)
-
-import imp
-context = imp.load_source('context', 'pycontext')
